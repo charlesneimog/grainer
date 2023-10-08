23a4099e4a3356d8c47278a4ba06f4d8b2eddd15
Charles K. Neimog <charlesneimog@outlook.com>
2023-10-08 14:15:37 -0300

    Add automatic builder for all plataforms and rename help to examples

--- src/grainer~.c
+++ src/grainer~.c
@@ -14,7 +14,7 @@ Improve the data structure (it works, but could be much simpler)
 */
 #include <m_pd.h>
 #include <math.h>
-#include <malloc.h>
+// #include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
